04b308a928d3d34a5ea3f39ae2efe7dceeca0db5
NorthernLord <berend.kalberg@gmail.com>
2019-05-23 15:41:22 +0200

    update tenserflow

--- App.js
+++ App.js
@@ -60,3 +60,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+
+// Tensorflow shite here
+
+import { TfImageRecognition } from 'react-native-tensorflow';
+ 
+const tfImageRecognition = new TfImageRecognition({
+  model: require('./assets/tenserflow_inception_graph.pb'),
+  labels: require('./assets/tensorflow_labels.txt'),
+  imageMean: 117, // Optional, defaults to 117
+  imageStd: 1 // Optional, defaults to 1
+})
+ 
+const results = tfImageRecognition.recognize({
+  image: require('./assets/test.jpg'),
+  inputName: "input", //Optional, defaults to "input"
+  inputSize: 224, //Optional, defaults to 224
+  outputName: "output", //Optional, defaults to "output"
+  maxResults: 3, //Optional, defaults to 3
+  threshold: 0.1, //Optional, defaults to 0.1
+})
+ 
+results.forEach(result =>
+  console.log(
+    result.id, // Id of the result
+    result.name, // Name of the result
+    result.confidence // Confidence value between 0 - 1
+  )
+)
+ 
+tfImageRecognition.close() // Necessary in order to release objects on native side
